0ec66d08d0c1cbd8832206f938bf6667bcfa0075
Manav Saini <itsmemanavsaini@gmail.com>
2024-07-21 12:23:26 +0530

    add controllers to modify , delete , and fetch

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/userInfo")
-  .then(() => console.log("db connected"))
-  .catch((err) => console.log("db connection err ", err));
-
 const userSchema = mongoose.Schema({
   userName: {
     type: String,
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,9 +8,7 @@ const{
     delete_product} = require('../controllers/product')
 
 
-// get all item in 
-router.route("/").post(add_product)
-// router.route("/").get(get_all_products).post(add_product)
+router.route("/").get(get_all_products).post(add_product)
 router.route("/:id").get(get_one_product).patch(update_product).delete(delete_product)
 
 module.exports = router
